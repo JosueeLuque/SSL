ade9597c3db27992412713986f9e3ebdd53a721d
Josue Luque <kluqueherbas@frba.utn.edu.ar>
2024-04-15 19:01:11 -0300

    simplified code

--- 00-CHelloWorld/hello.c
+++ 00-CHelloWorld/hello.c
@@ -1,26 +1,30 @@
 #include <stdio.h>
 
-void crearArchivoSalida(const char *mensaje, const char *nombreArchivo);
+// void crearArchivoSalida(const char *mensaje, const char *nombreArchivo);
 
 int main()
 {
     char saludo[] = "Hello, World! My name is Josué Luque\n";
     printf(saludo);
 
-    crearArchivoSalida(saludo, "output.txt");
+    // crearArchivoSalida(saludo, "output.txt");
     return 0;
 }
 
-// Función crearArchivoSalida -> crea un archivo "output.txt" y escribe el saludo pasado
-void crearArchivoSalida(const char *mensaje, const char *nombreArchivo)
-{
-    FILE *fp;
-    fp = fopen(nombreArchivo, "w");
-    if (fp == NULL)
+/*
+    # Genera archivo output.txt utlizando manejo de archivos
+
+    * Función crearArchivoSalida -> crea un archivo "output.txt" y escribe el saludo pasado
+    void crearArchivoSalida(const char *mensaje, const char *nombreArchivo)
     {
-        printf("Error al abrir el archivo: %s\n", nombreArchivo);
-        return;
+        FILE *fp;
+        fp = fopen(nombreArchivo, "w");
+        if (fp == NULL)
+        {
+            printf("Error al abrir el archivo: %s\n", nombreArchivo);
+            return;
+        }
+        fprintf(fp, mensaje);
+        fclose(fp);
     }
-    fprintf(fp, mensaje);
-    fclose(fp);
-}
+*/
